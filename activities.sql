847a8ad1008f9f6f61e763ad97e4019b9da1dbc4
Angela Zhou <36517335+angela-zhou@users.noreply.github.com>
2021-01-16 16:07:58 -0500

    Add files via upload
    Changed Column names, added one row of date ideas, added an auto-increment idea number

--- activities.sql
+++ activities.sql
@@ -1,5 +1,31 @@
 CREATE DATABASE activities;
 USE activities;
-CREATE TABLE suggestions(category TEXT, state TEXT);
-SELECT * FROM suggestions;
-INSERT INTO suggestions VALUES ('Skating', 'Outdoors');
+
+CREATE TABLE GoOut (
+	IdeaNumber int NOT NULL AUTO_INCREMENT,
+    StayActive varchar(255),
+    GetCreative varchar(255),
+    Foodie varchar(255),
+    GetCosy varchar(255),
+    GameNight varchar(255),
+    SupportLocal varchar(255),
+    WildCard varchar(255),
+    PRIMARY KEY (IdeaNumber)
+);
+
+CREATE TABLE StayHome (
+	IdeaNumber int NOT NULL AUTO_INCREMENT,
+    StayActive varchar(255),
+    GetCreative varchar(255),
+    Foodie varchar(255),
+    GetCosy varchar(255),
+    GameNight varchar(255),
+    WildCard varchar(255),
+    PRIMARY KEY (IdeaNumber)
+);
+
+INSERT INTO GoOut (StayActive, GetCreative, Foodie, GetCosy, GameNight, SupportLocal, WildCard)
+VALUES ('Hiking', 'Pottery Painting', 'Cooking Class', 'Go to a Hot Spring', 'Escape Room', 'Museum', 'Axe Throwing');
+
+INSERT INTO StayHome (StayActive, GetCreative, Foodie, GetCosy, GameNight, WildCard)
+VALUES ('Yoga', 'Painting and Colouring', 'Baking', 'Watch a Movie/TV Show', 'Play Board Games', 'Make a mini garden');
