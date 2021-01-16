7fb2dae4c27166f345602f418060eb62e8b19bf4
Angela Zhou <36517335+angela-zhou@users.noreply.github.com>
2021-01-16 16:57:34 -0500

    Add files via upload
    Added more rows

--- activities.sql
+++ activities.sql
@@ -27,5 +27,24 @@ CREATE TABLE StayHome (
 INSERT INTO GoOut (StayActive, GetCreative, Foodie, GetCosy, GameNight, SupportLocal, WildCard)
 VALUES ('Hiking', 'Pottery Painting', 'Cooking Class', 'Go to a Hot Spring', 'Escape Room', 'Museum', 'Axe Throwing');
 
+INSERT INTO GoOut (StayActive, GetCreative, Foodie, GetCosy, GameNight, SupportLocal, WildCard)
+VALUES ('Skating', 'Pottery Class', 'Go to a Fancy Restaurant', 'Spa Day', 'Arcade', 'Art Gallery', 'Go to a Zoo'); 
+
+INSERT INTO GoOut (StayActive, GetCreative, Foodie, GetCosy, GameNight, SupportLocal, WildCard)
+VALUES ('Dance Class', 'Painting Class', 'Wine & Cheese Tasting', "Movie Theatre", 'Fair', 'Go to a Fundraising Event', 'Hot Air Balloon Ride'); 
+
+INSERT INTO GoOut (StayActive, GetCreative, Foodie, GetCosy, GameNight, SupportLocal, WildCard)
+VALUES ('Goat Yoga', 'Make gift baskets for each other', 'Go to a Retro Restaurant', 'Drive-In Theatre', 'Laser Tag', 'Go thrifting', 'Go to a random concert'); 
+
+
 INSERT INTO StayHome (StayActive, GetCreative, Foodie, GetCosy, GameNight, WildCard)
 VALUES ('Yoga', 'Painting and Colouring', 'Baking', 'Watch a Movie/TV Show', 'Play Board Games', 'Make a mini garden');
+
+INSERT INTO StayHome (StayActive, GetCreative, Foodie, GetCosy, GameNight, WildCard)
+VALUES ('Water Balloon Fight', 'Sharpie Mug Drawing', 'Cook a Multiple Course Meal', 'Spa Day', 'Play Chess', 'DIY Science Experiment'); 
+
+INSERT INTO StayHome (StayActive, GetCreative, Foodie, GetCosy, GameNight, WildCard)
+VALUES ('Nerf Gun Fight', 'Make Shrinky Dinks', 'Make mug cakes', 'Build a fort', 'Learn Card Tricks', 'Have a Photoshoot'); 
+
+INSERT INTO StayHome (StayActive, GetCreative, Foodie, GetCosy, GameNight, WildCard)
+VALUES ('Backyard Camping', 'Learn to crochet', 'Decorate sugar cookies', 'Write love letters to each other', 'Play Card Games', 'Put together a puzzle'); 
