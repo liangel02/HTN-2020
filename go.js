f712986d1f3273c5394fc91c47283443261b5d71
kayamak38 <66970479+kayamak38@users.noreply.github.com>
2021-01-17 00:36:08 -0500

    Add files via upload

---
+++ go.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import { Button } from "react-native";
+
+const active = ({ navigation }) => {
+  return (
+    <Button
+      title = "Stay Active"
+    />
+    );
+}
+const creative = ({ navigation }) => {
+  return (
+    <Button
+      title = "Get Creative"
+    />
+    );
+}
+const foodie = ({ navigation }) => {
+  return (
+    <Button
+      title = "Foodie"
+    />
+    );
+}
+const Cozy = ({ navigation }) => {
+  return (
+    <Button
+      title = "Get Cozy"
+    />
+    );
+}
+const gameNight = ({ navigation }) => {
+  return (
+    <Button
+      title = "Game Night"
+    />
+    );
+}
+const supportLocal = ({ navigation }) => {
+  return (
+    <Button
+      title = "Support Local"
+    />
+    );
+}
+const wildCard = ({ navigation }) => {
+  return (
+    <Button
+      title = "Wild Card"
+    />
+    );
+}
+
+
+export default GoOut;
